3be0f322b7f5964fd82f324e5b03104c912a8197
jusfr <jusfr.v@gmail.com>
2016-05-31 13:52:42 +0800

    Place IEnumerable<TResult> Retrive(Object[] keys) in IRepository

--- src/Chuye.Persistent/IRepository.cs
+++ src/Chuye.Persistent/IRepository.cs
@@ -13,6 +13,7 @@ namespace Chuye.Persistent {
         IQueryable<TResult> All { get; }
         TReutrn Fetch<TReutrn>(Func<IQueryable<TResult>, TReutrn> query);
         TResult Retrive(Object id);
+        IEnumerable<TResult> Retrive(Object[] keys);
         IEnumerable<TResult> Retrive<TKey>(String field, params TKey[] keys);
     }
 }
