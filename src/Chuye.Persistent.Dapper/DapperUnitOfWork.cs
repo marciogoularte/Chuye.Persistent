85103dd7e00f29524dfe11eb8eef9a244356b7a6
Jusfrw <jusfr.v@gmail.com>
2016-10-17 09:53:18 +0800

    Set IDbConnection as public

--- src/Chuye.Persistent.Dapper/DapperUnitOfWork.cs
+++ src/Chuye.Persistent.Dapper/DapperUnitOfWork.cs
@@ -15,6 +15,10 @@ namespace Chuye.Persistent.Dapper {
         private readonly IDbConnection _connection;
         private Int32 opend;
 
+        public IDbConnection Connection {
+            get { return _connection; }
+        }
+
         public DapperUnitOfWork(IDbConnection connection) {
             _connection = connection;
         }
