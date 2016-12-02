28fc5b09cacbe7e1b4e945107223f8169cd26489
Jusfrw <jusfr.w@gmail.com>
2016-12-02 15:52:27 +0800

    Change namespace of HybridPetaPocoUnitOfWork

--- src/Chuye.Persistent.NHibernate/PetaPoco/HybridPetaPocoUnitOfWork.cs
+++ src/Chuye.Persistent.NHibernate/PetaPoco/HybridPetaPocoUnitOfWork.cs
@@ -3,9 +3,9 @@ using System.Data;
 using System.Data.Common;
 using System.Diagnostics;
 using System.Threading;
-using Chuye.Persistent.NHibernate;
+using Chuye.Persistent.PetaPoco;
 
-namespace Chuye.Persistent.PetaPoco {
+namespace Chuye.Persistent.NHibernate.PetaPoco {
     public class HybridPetaPocoUnitOfWork : PetaPocoUnitOfWork {
         public HybridPetaPocoUnitOfWork(NHibernateUnitOfWork uow)
             : base(uow.OpenSession().Connection) {
