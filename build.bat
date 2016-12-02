d862d41857de959b74201ef871c6607729713852
Jusfrw <jusfr.w@gmail.com>
2016-12-02 10:30:58 +0800

    Combin Chuye.Persistent.PetaPoco into Chuye.Persistent, add HybridPetaPocoUnitOfWork

--- build.bat
+++ build.bat
@@ -6,20 +6,16 @@
 
 :build
 msbuild "%local%\Chuye.Persistent.sln" /t:Rebuild /P:Configuration=Release
-rem msbuild "%local%src\Chuye.Persistent.NHibernate\Chuye.Persistent.NHibernate.csproj" /t:Rebuild /P:Configuration=Release
-rem msbuild "%local%src\Chuye.Persistent.PetaPoco\Chuye.Persistent.PetaPoco.csproj" /t:Rebuild /P:Configuration=Release
 @goto copy
 
 :copy
 robocopy "%local%src\Chuye.Persistent\bin\Release" "%local%release\Chuye.Persistent" /mir
-robocopy "%local%src\Chuye.Persistent.PetaPoco\bin\Release" "%local%release\Chuye.Persistent.PetaPoco" /mir
 robocopy "%local%src\Chuye.Persistent.NHibernate\bin\Release" "%local%release\Chuye.Persistent.NHibernate" /mir
 @goto pack
 
 :pack
 @pushd "%local%"
 .nuget\NuGet.exe pack build\Chuye.Persistent.nuspec -Prop Configuration=Release -OutputDirectory release
-.nuget\NuGet.exe pack build\Chuye.Persistent.PetaPoco.nuspec -Prop Configuration=Release -OutputDirectory release
 .nuget\NuGet.exe pack build\Chuye.Persistent.NHibernate.nuspec -Prop Configuration=Release -OutputDirectory release
 @goto end
 
